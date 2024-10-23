417694860ce9838ce297acefa88a3c6d0a9bd617
Jyotirajsutar <141478954+Jyotirajsutar@users.noreply.github.com>
2024-10-23 21:08:06 +0530

    add two files, 03-variabeAssignment.js & 04-variableAssignment.js
    add two files, 03-variabeAssignment.js & 04-variableAssignment.js

---
+++ 03-variableAssignment.js
@@ -0,0 +1,18 @@
+var city = "Bhubaneswar";
+console.log("City: ", city);
+
+var bestFriend = "Goutam & Munu";
+console.log("Best Friend: ", bestFriend);
+
+var country = "INDIA";
+console.log("Country: ", country);
+
+var school = "Talakushuma High School";
+console.log("School:", school);
+
+var college = "LN College";
+console.log("College:", college);
+
+
+
+
---
+++ 04-variableAssignment.js
@@ -0,0 +1,35 @@
+var myFullName;
+console.log("My Name is:", myFullName);
+
+myFullName = "Jyotiraj Sutar";
+console.log("My Name is:", myFullName);
+
+var firstName = "Jyotiraj";
+var lastName = "Sutar";
+console.log("My first Name is:", firstName);
+console.log("My last Name is:", lastName);
+
+console.log("-------------------------------");
+
+var myLove = "I Love Only JS";
+console.log("Language:", myLove);
+
+console.log("-------------------------------");
+var age = 21;
+var address = "Bangari";
+var pin = 754153;
+var gender = "Male";
+var isMarried = false;
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName );
+console.log("Age:", age);
+console.log("Address:", address);
+console.log("PinCode:", pin);
+console.log("Gender:", gender);
+console.log("isMarried:", isMarried);
+
+
+
+
+
+
