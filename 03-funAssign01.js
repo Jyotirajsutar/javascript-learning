8c71c80bb174e8d5f6ffeaf0d76904feab14ec35
Jyotirajsutar <141478954+Jyotirajsutar@users.noreply.github.com>
2024-10-24 23:10:53 +0530

    addThisFile03-funAnssign.js
    addThisFile03-funAnssign.js

---
+++ 03-funAssign01.js
@@ -0,0 +1,28 @@
+
+function hello() {
+    console.log("I Love My Country...");
+
+}
+hello();
+function world() {
+    console.log("My Country Is INDIA.");
+    console.log("-----------------------");
+}
+world();
+
+function personalDetails(firstName, lastName, collegeName) {
+    console.log("My First Name is:", firstName);
+    console.log("My Last Name is:", lastName);
+    console.log("My College Name is:", collegeName);
+    console.log("----------------------------------");
+}
+personalDetails('Jyotiraj', 'Sutar', 'LN College');
+
+function addThreeValues(n1, n2, n3) {
+    console.log("The Value Parameters:", n1, n2, n3);
+    var result = n1 + n2 + n3;
+    console.log("The result is:", result);
+    console.log("----------------------------------");
+}
+addThreeValues(10.23, 600, 40);
+addThreeValues("Hello", "Good", "Morning");
