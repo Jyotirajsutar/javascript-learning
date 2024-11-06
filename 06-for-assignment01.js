79e9917bb3df4d02cf54e727279905a0df3e5654
Jyotirajsutar <141478954+Jyotirajsutar@users.noreply.github.com>
2024-11-06 21:15:07 +0530

    add-06-forAssignment.js file
    add-06-forAssignment.js file

---
+++ 06-for-assignment01.js
@@ -0,0 +1,36 @@
+
+
+console.log("==Print numbers from 5 to 15 by increamenting 1==");
+for (let i = 5; i<=15; i++) {
+    console.log(i);
+}
+
+console.log("==Print numbers from 50 to 40. decreamenting by 1==");
+for (let i = 50; i>=40; i--) {
+    console.log(i);
+}
+
+console.log("==Print first 15 odd numbers==");
+for (let i = 1; i<30; i=i+2) {
+    console.log(i);
+}
+
+console.log("==Print first 10 even numbers==");
+for (let i=2; i<=20; i=i+2) {
+    console.log(i);
+}
+
+console.log("==Print Table of 5...==");
+for (var i=5; i<=50; i= i+5) {
+    console.log(i);
+}
+
+console.log("==Print Table of 10...==");
+for (var i=10; i<=100; i= i+10) {
+    console.log(i);
+}
+
+console.log("==Print table of 10 in reverse order==");
+for (let i=100; i>=10; i=i-10) {
+    console.log(i);
+}
