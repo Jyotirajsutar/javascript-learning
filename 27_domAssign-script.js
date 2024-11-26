465cbb6c1d7921fa17e17493f39437c4c12df143
Jyotirajsutar <141478954+Jyotirajsutar@users.noreply.github.com>
2024-11-26 21:21:20 +0530

    Add  27_domAssign.html and 27_domAssign-script.js two files
    add two files

---
+++ 27_domAssign-script.js
@@ -0,0 +1,18 @@
+
+console.log("=========== Main Heading ===============");
+const mainHeading = document.querySelector("#h1");
+console.log(mainHeading);
+// console.log(mainHeading.innerHTML);
+
+console.log("=========== Second Advantage =============== ");
+const secondAdvantage = document.querySelector("#Available");
+console.log(secondAdvantage);
+console.log(secondAdvantage.innerHTML);
+
+console.log("=========== Advantages Div =============== ");
+const addDiv = document.querySelector("#advantages");
+console.log(addDiv);
+console.log(secondAdvantage.innerHTML);
+// for (const Div of addDiv) {
+//     console.log(Div.innerHTML);
+// }
